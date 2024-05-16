61f2ea6ef512859caebc0978b0d1859d8bb037f1
Zain541 <zainfarooq541@gmail.com>
2024-05-16 06:16:36 +0500

    added page title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ $title ?? 'Form' }}</title>
     </head>
     <body>
         {{ $slot }}
